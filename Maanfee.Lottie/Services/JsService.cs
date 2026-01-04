6adf605bc1a07bbb3a719b580c93c23dd85d9362
Maanfee <Maanfee@outlook.com>
2026-01-04 10:28:30 +0330

    04-10-14

--- Maanfee.Lottie/Services/JsService.cs
+++ Maanfee.Lottie/Services/JsService.cs
@@ -1,25 +1,27 @@
 ﻿using Microsoft.JSInterop;
 
-namespace Maanfee.JsServices
+namespace Maanfee.Lottie
 {
     public abstract class JsService : IAsyncDisposable
     {
-        public JsService(IJSRuntime JsRuntime, string NameSpace)
+        public JsService(IJSRuntime JsRuntime, string NameSpace, string JsFileName = "JsInterop.js")
         {
             _JSRuntime = JsRuntime ?? throw new ArgumentNullException(nameof(JsRuntime));
             _NameSpace = NameSpace ?? throw new ArgumentNullException(nameof(NameSpace));
+            _JsFileName = JsFileName ?? throw new ArgumentNullException(nameof(JsFileName));
         }
 
         protected readonly IJSRuntime _JSRuntime;
         protected IJSObjectReference _Module;
         protected bool IsDisposed = false;
         private readonly string _NameSpace = string.Empty;
+        private readonly string _JsFileName = string.Empty;
 
         protected virtual async Task EnsureModuleLoaded()
         {
             if (_Module == null)
             {
-                _Module = await _JSRuntime.InvokeAsync<IJSObjectReference>("import", $"./_content/{_NameSpace}/js/JsInterop.js");
+                _Module = await _JSRuntime.InvokeAsync<IJSObjectReference>("import", $"./_content/{_NameSpace}/js/{_JsFileName}");
 
                 // اطمینان از بارگذاری وابستگی‌ها
                 await _Module.InvokeVoidAsync("ensureDependencies");
--- Maanfee.Lottie/Services/LottieService.cs
+++ Maanfee.Lottie/Services/LottieService.cs
@@ -1,5 +1,4 @@
-﻿using Maanfee.JsServices;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 
 namespace Maanfee.Lottie
